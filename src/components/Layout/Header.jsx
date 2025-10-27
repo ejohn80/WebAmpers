7f0defd81b8314c9e13e24a1426b77c7c844e792
Tomas Zmiejevas <tomas.zmiejevas@gmail.com>
2025-10-26 23:02:08 -0700

    Implemented project creation/fetching logic + sidebar UI

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import DraggableDiv from '../Generic/DraggableDiv';
 import AudioImportButton from '../AudioImport/AudioImportButton';
+import { logout } from '../Auth/AuthUtils';
+import { useUserData } from '../../hooks/useUserData';
+import { useNavigate } from 'react-router-dom';
 
 /**
  * Header component for the application layout.
@@ -10,14 +13,19 @@ import AudioImportButton from '../AudioImport/AudioImportButton';
  * @param {function} props.onImportError - Callback for failed audio import.
  */
 function Header({ onImportSuccess, onImportError }) {
+  const navigate = useNavigate();
+  const { userData, loading } = useUserData();
+
+
   return (
     <DraggableDiv color="red">
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%', margin: '0' }}>
         <span>**Header** (Red Section)</span>
         <AudioImportButton 
           onImportSuccess={onImportSuccess}
           onImportError={onImportError}
         />
+        {userData ? <button onClick={logout}>Log out</button> : <button onClick={() => navigate('/register')}>Login or Register</button>}
       </div>
     </DraggableDiv>
   );
